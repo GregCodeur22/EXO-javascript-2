82921d4af58a358b161c8e4dfc7f07fcad51516e
Grégory <gregory.arside@gmail.com>
2022-04-22 16:27:53 +0200

    test unitaire

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
-const x = prompt("choisisé un nombre ");
+// const x = prompt("choisisé un nombre ");
 
-for (let i = 0; i < 10; i++) {
-  let result = [i] * x;
-  console.log(i, "*", x, "=", result);
+// for (let i = 0; i < 10; i++) {
+//   let result = [i] * x;
+//   console.log(i, "*", x, "=", result);
+// }
+
+let root = document.getElementById("root");
+
+let age = prompt("Quel est votre age ?");
+
+if (!isNaN(age)) {
+  if (age > 18) {
+    root.innerHTML = "vous êtes majeur ";
+  } else {
+    root.innerHTML = "vous êtes mineur ";
+  }
+} else {
+  root.innerHTML = "ce n'est pas un nombre ";
 }
